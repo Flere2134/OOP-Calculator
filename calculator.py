369b64602ef661f46eaa5ba1de6589c94aa66a9c
Flere21 <aaronhans2134@gmail.com>
2023-05-21 11:14:16 +0800

    pseudocode

--- calculator.py
+++ calculator.py
@@ -1,1 +1,16 @@
 class Calculator:
+#method for addition
+#method for subtraction
+#method for multiplication
+#method for division
+#method of main calculator
+#ask user input 1st number
+#ask user input of operation
+#ask user input 2nd number
+#if add, add 2 numbers
+#if minus, subtract 2 numbers
+#if multiply, multiply 2 numbers
+#if divide, divide 2 numbers
+#print output
+#try again feature
+#exception handling for invalid inputs
