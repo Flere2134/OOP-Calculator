08e9bbac5e0ff21d2e3a6125ea284494afb37b52
Flere21 <aaronhans2134@gmail.com>
2023-05-21 11:18:21 +0800

    methods for arithmethic operations

--- calculator.py
+++ calculator.py
@@ -1,8 +1,16 @@
 class Calculator:
 #method for addition
+    def add(self, num_1, num_2):
+        return num_1 + num_2
 #method for subtraction
+    def subtract(self, num_1, num_2):
+        return num_1 - num_2
 #method for multiplication
+    def multiply(self, num_1, num_2):
+        return num_1 * num_2
 #method for division
+    def divide(self, num_1, num_2):
+        return num_1 / num_2
 #method of main calculator
 #ask user input 1st number
 #ask user input of operation
